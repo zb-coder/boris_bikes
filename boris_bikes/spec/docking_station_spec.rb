4eb1163ef4e694e4d5aba16fb337d4efb9bbec9b
zainab baruud <zainabbaruud@macbook-air.home>
2021-07-28 17:00:42 +0100

    Added rspec test that responds to release_bike

--- boris_bikes/spec/docking_station_spec.rb
+++ boris_bikes/spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
-
-end
+    # it 'responds to release_bike' do
+    # expect(subject).to respond_to :release_bike
+    it { is_expected.to respond_to :release_bike}
+  end
